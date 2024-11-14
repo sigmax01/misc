dbfdccb3273f2a058960d64c91d696a41ddd5944
wenzexu <ricol.xwz@outlook.com>
2024-11-14 19:38:07 +1100

    docs: update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "vitepress";
 import { nav } from "./nav.mjs";
 import { sidebar } from "./sidebar.mjs";
-import { analysis } from "./analysis";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
